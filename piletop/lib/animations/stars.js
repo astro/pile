968c808ead9196d598876912fda41e38af436e36
Astro <astro@spaceboyz.net>
2014-11-05 01:01:45 +0100

    randomize stars animation parameters

--- piletop/lib/animations/stars.js
+++ piletop/lib/animations/stars.js
@@ -1,4 +1,7 @@
 module.exports = function(presets) {
+    var star_ttl = 200 + 1800 * Math.random();
+    var star_density = 0.0001 + 0.0009 * Math.random();
+    var colorful = Math.random() < 0.3;
     var stars = [];
 
     return {
@@ -9,18 +12,25 @@ module.exports = function(presets) {
             }
 
             var now = Date.now();
-            if (Math.random() < (output.x * output.y / 1000)) {
+            if (Math.random() < (output.x * output.y * star_density)) {
                 stars.push({
                     x: Math.floor(output.x * Math.random()),
                     y: Math.floor(output.y * Math.random()),
-                    birth: now
+                    birth: now,
+                    r: 127 + 128 * Math.random(),
+                    g: 127 + 128 * Math.random(),
+                    b: 127 + 128 * Math.random()
                 });
             }
             stars.forEach(function(star) {
                 var i = star.y * output.x + star.x;
-                star.health = 255 * (1 - (now - star.birth) / 500);
+                star.health = 255 * (1 - (now - star.birth) / star_ttl);
                 var brightness = Math.max(frame[i][0], star.health);
-                frame[i] = [brightness, brightness, brightness];
+                if (colorful) {
+                    frame[i] = [star.r * brightness, star.g * brightness, star.b * brightness];
+                } else {
+                    frame[i] = [brightness, brightness, brightness];
+                }
             });
             stars = stars.filter(function(star) {
                 return star.health > 0;
