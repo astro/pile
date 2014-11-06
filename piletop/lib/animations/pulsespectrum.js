755feff576b3c27cd959cb4ada42fff8861d915d
Astro <astro@spaceboyz.net>
2014-11-06 23:46:02 +0100

    pulsespectrum: colorize

--- piletop/lib/animations/pulsespectrum.js
+++ piletop/lib/animations/pulsespectrum.js
@@ -1,15 +1,16 @@
 var pulsefreqs = require("../pulsefreqs");
 var through = require('through2');
 
-var freqs;
-var maxIntensity = 1000;
-var maxFreq = 3200;
+var bins;
+var maxIntensity = 1;
+var maxFreq = 3000;
 pulsefreqs(function(err, src) {
     console.log("pulse src");
-    src.pipe(through.obj(function(freqs_, enc, cb) {
-        freqs = freqs_;
-        for(var i = 0; i < freqs.length; i++) {
-            var intensity = Math.log(freqs[i][1]);
+    maxIntensity *= 0.95;
+    src.pipe(through.obj(function(freqs, enc, cb) {
+        bins = binify(freqs, maxFreq, 160);
+        for(var i = 0; i < bins.length; i++) {
+            var intensity = bins[i];
             if (intensity > maxIntensity) {
                 maxIntensity = intensity;
                 console.log("maxIntensity", maxIntensity);
@@ -24,17 +25,12 @@ module.exports = function(presets) {
         render: function(output, cb) {
             var frame = [];
             for(var x = 0; x < output.x; x++) {
-                var hue = 360 * x / output.x;
-                var val = 0;
                 var freq1 = maxFreq * x / output.x;
                 var freq2 = maxFreq * (x + 1) / output.x;
-                var intensity = 0;
-                for(var i = 0; freqs && i < freqs.length; i++) {
-                    if (freqs[i][0] >= freq1 && freqs[i][0] < freq2) {
-                        val = Math.max(intensity, freqs[i][1] / maxIntensity);
-                    }
-                    // console.log({ x: x, f1: freq1, f2: freq2, v: val });
-                }
+                var intensity = bins && bins[x] || 0;
+                var val = Math.max(0, Math.log(intensity) / Math.log(maxIntensity));
+                var hue = Math.min(360, 360 * (1 - Math.log(intensity) / Math.log(maxIntensity)));
+
                 var rgb = HSVtoRGB(hue, 1, val);
                 var color = [rgb.r, rgb.g, rgb.b];
                 for(var y = 0; y < output.y; y++) {
@@ -46,6 +42,23 @@ module.exports = function(presets) {
         }
     };
 };
+
+function binify(freqs, maxFreq, count) {
+    var bins = [];
+    for(var x = 0; x < count; x++) {
+        var freq1 = maxFreq * x / count;
+        var freq2 = maxFreq * (x + 1) / count;
+        var intensity = 0;
+        for(var i = 0; freqs && i < freqs.length; i++) {
+            var freq = freqs[i][0];
+            if (freq >= freq1 && freq < freq2) {
+                intensity += freqs[i][1];
+            }
+        }
+        bins.push(intensity);
+    }
+    return bins;
+}
 
 function HSVtoRGB(h, s, v) {
     // this may look a bit nerdy, but everything is just
